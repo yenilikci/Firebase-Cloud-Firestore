ac2ee3eb5b1dd7e14df32167a469707a19546abe
Melih Çelik <57464067+yenilikci@users.noreply.github.com>
2021-05-24 05:20:34 +0300

    Firestore Veri Ekleme

--- script.js
+++ script.js
@@ -1,14 +1,49 @@
-//console.log(db.collection('haberler'));
+ // Your web app's Firebase configuration
+ var firebaseConfig = {
+     apiKey: "burasıgizlendi",
+     authDomain: "modern-js-2e1db.firebaseapp.com",
+     projectId: "modern-js-2e1db",
+     storageBucket: "modern-js-2e1db.appspot.com",
+     messagingSenderId: "burasıgizlendi",
+     appId: "burasıgizlendi"
+ };
+ // Initialize Firebase
+ firebase.initializeApp(firebaseConfig);
+ const db = firebase.firestore();
 
-db.collection('haberler').get().then(snapshot => {
-    //console.log(snapshot);
-    //console.log(snapshot.docs[0].data());
-    snapshot.docs.forEach((doc) => {
-        //console.log(doc.data());    
-        console.log(doc.data().ad);
-        console.log(doc.data().tarih.toDate());
-    });
+ //console.log(db.collection('haberler'));
 
-}).catch((err) => {
-    console.log(err);
-})
+
+ //verilere erişme
+ db.collection('haberler').get().then(snapshot => {
+     //console.log(snapshot);
+     //console.log(snapshot.docs[0].data());
+     snapshot.docs.forEach((doc) => {
+         //console.log(doc.data());    
+         console.log(doc.data().ad);
+         console.log(doc.data().tarih.toDate());
+     });
+
+ }).catch((err) => {
+     console.log(err);
+ })
+
+
+ //veri ekleme
+ const button = document.querySelector('button');
+ button.addEventListener('click', () => {
+
+     const now = new Date();
+     var haber = {
+         ad: 'haber 3',
+         icerik: 'içerik 3',
+         tarih: firebase.firestore.Timestamp.fromDate(now)
+     };
+
+     db.collection('haberler').add(haber).then(() => {
+         console.log('haber eklendi');
+     }).catch(() => {
+         console.log('ekleme işlemi başarısız');
+     })
+
+ })
